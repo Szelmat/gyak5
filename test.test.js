707e9f3a4127a451038a00014a675ee4bebc0102
Széles Máté <szelessm@gmail.com>
2021-10-07 11:18:18 +0200

    add tests for payBills

--- test.test.js
+++ test.test.js
@@ -27,5 +27,34 @@ describe('tests for the ATM class', () => {
         test('balance should be 3000', () => {
             expect(myATM.getBalance()).toBe(3000);
         });
+
+        test('putIn should work', () => {
+            expect(myATM.getBalance()).toBe(3000);
+            myATM.putIn(500);
+            expect(myATM.getBalance()).toBe(3500);
+        });
+    });
+    describe('tests for the payBills method', () => {
+        let myATM;
+        beforeEach(() => {
+            myATM = new ATM(3000);
+        });
+
+        test('balance should be 3000', () => {
+            expect(myATM.getBalance()).toBe(3000);
+        });
+
+        test('payBills should work', () => {
+            expect(myATM.getBalance()).toBe(3000);
+            myATM.payBills();
+            expect(myATM.getBalance()).toBe(3000 - myATM.bills);
+        });
+
+        test('payBills() x2 should work', () => {
+            expect(myATM.getBalance()).toBe(3000);
+            myATM.payBills();
+            myATM.payBills();
+            expect(myATM.getBalance()).toBe(3000 - (2 * myATM.bills));
+        });
     });
 });
